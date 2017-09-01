b2b24841c8da88a304d201528613a841b3fa49d9
akorn3001 <akorn3000@gmail.com>
2017-09-01 11:16:15 -0400

    Change city image for Ba Sing Se

--- frontend/components/dummy_cities.js
+++ frontend/components/dummy_cities.js
@@ -8,7 +8,7 @@ const DUMMY_CITIES = [
   {id: 5, name: 'Miami', url: 'https://d12dkjq56sjcos.cloudfront.net/pub/media/wysiwyg/miami/01-city-landing/Miami-Skyline-Sunset-Big-Bus-Tours-12-01-17.jpg'},
   {id: 6, name: 'Berlin', url: 'https://a1.odistatic.net/images/landingpages/vacation/1920x800/berlin_1920x800.jpg'},
   {id: 7, name: 'Gravity Falls', url: 'http://vignette2.wikia.nocookie.net/gravityfalls/images/6/68/S1e20_ian_worrel_town_day.jpg/revision/latest?cb=20130830073132'},
-  {id: 8, name: 'Ba Sing Se', url: 'https://vignette2.wikia.nocookie.net/avatar/images/f/f7/Modern_Ba_Sing_Se.png/revision/latest?cb=20140730154445'},
+  {id: 8, name: 'Ba Sing Se', url: 'https://theartofthestorydotcom.files.wordpress.com/2015/01/korra_basingse_outerring.jpg'},
   {id: 1000, name: 'New New York', url: 'http://vignette2.wikia.nocookie.net/en.futurama/images/5/55/160_planet-express-building_1024.png/revision/latest?cb=20090521203416'},
   {id: 9, name: 'Seattle', url: 'http://www.sustainableseattle.org/wp-content/uploads/2015/08/SeattleMunicipleArchive-e1488676312333.jpg'},
   {id: 10, name: 'Rome', url: 'https://brightcove04pmdo-a.akamaihd.net/5104226627001/5104226627001_5232386545001_5215063851001-vs.jpg?pubId=5104226627001&videoId=5215063851001'},
