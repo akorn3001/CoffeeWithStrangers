1f87d92df45c334bba8daac191792e2cec433a42
akorn3001 <akorn3000@gmail.com>
2017-09-01 11:14:20 -0400

    Add 2 more cities to page

--- frontend/components/dummy_cities.js
+++ frontend/components/dummy_cities.js
@@ -10,7 +10,9 @@ const DUMMY_CITIES = [
   {id: 7, name: 'Gravity Falls', url: 'http://vignette2.wikia.nocookie.net/gravityfalls/images/6/68/S1e20_ian_worrel_town_day.jpg/revision/latest?cb=20130830073132'},
   {id: 8, name: 'Ba Sing Se', url: 'https://vignette2.wikia.nocookie.net/avatar/images/f/f7/Modern_Ba_Sing_Se.png/revision/latest?cb=20140730154445'},
   {id: 1000, name: 'New New York', url: 'http://vignette2.wikia.nocookie.net/en.futurama/images/5/55/160_planet-express-building_1024.png/revision/latest?cb=20090521203416'},
-  {id: 9, name: 'Seattle', url: 'http://www.sustainableseattle.org/wp-content/uploads/2015/08/SeattleMunicipleArchive-e1488676312333.jpg'}
+  {id: 9, name: 'Seattle', url: 'http://www.sustainableseattle.org/wp-content/uploads/2015/08/SeattleMunicipleArchive-e1488676312333.jpg'},
+  {id: 10, name: 'Rome', url: 'https://brightcove04pmdo-a.akamaihd.net/5104226627001/5104226627001_5232386545001_5215063851001-vs.jpg?pubId=5104226627001&videoId=5215063851001'},
+  {id: 11, name: 'Phoenix', url: 'http://www.beautyschoolsdirectory.com/sites/default/files/2017-03/Phoenix%2C%20AZ.jpg'}
 ];
 
 export default DUMMY_CITIES;
