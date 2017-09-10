bade161b7c7ec38b9e1d51c7aa0dbd989274dd1d
akorn3001 <akorn3000@gmail.com>
2017-09-10 18:36:49 -0400

    Add favicon, remove facebook and twitter links from footer

--- frontend/components/users/user_show_container.jsx
+++ frontend/components/users/user_show_container.jsx
@@ -12,7 +12,7 @@ const mapStateToProps = state => {
   };
 };
 
-const mapDispatchToProps = state => {
+const mapDispatchToProps = dispatch => {
   return {
     requestSingleUser: (userId) => dispatch(requestSingleUser(userId))
   };
