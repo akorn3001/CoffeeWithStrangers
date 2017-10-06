5c567329ffc57a6840148734017c8d0eacf37091
Alex Kornfeld <akorn3000@gmail.com>
2017-10-05 22:03:02 -0400

    Clean up seed data, upload city images to AWS

--- frontend/components/dummy_cities.js
+++ frontend/components/dummy_cities.js
@@ -1,18 +1,18 @@
 
 
 const DUMMY_CITIES = [
-  {id: 1, name: 'Los Angeles', url: 'http://finance.lacity.org/sites/g/files/wph641/f/styles/regular_hero/public/LA_Pano_0.JPG?itok=9DIiiZDv'},
-  {id: 2, name: 'New York City', url: 'https://a.travel-assets.com/findyours-php/viewfinder/images/res60/88000/88459-Brooklyn-Bridge.jpg'},
-  {id: 3, name: 'Honolulu', url: 'https://www.choicehotels.com/cms/images/choice-hotels/citys/420281-Honolulu/420281-Honolulu.jpg'},
-  {id: 4, name: 'Paris', url: 'http://aloha.fr/wp-content/uploads/2017/04/visiter-paris-gratuit.jpg'},
-  {id: 5, name: 'Miami', url: 'https://d12dkjq56sjcos.cloudfront.net/pub/media/wysiwyg/miami/01-city-landing/Miami-Skyline-Sunset-Big-Bus-Tours-12-01-17.jpg'},
-  {id: 6, name: 'Berlin', url: 'https://a1.odistatic.net/images/landingpages/vacation/1920x800/berlin_1920x800.jpg'},
-  {id: 7, name: 'Gravity Falls', url: 'http://vignette2.wikia.nocookie.net/gravityfalls/images/6/68/S1e20_ian_worrel_town_day.jpg/revision/latest?cb=20130830073132'},
-  {id: 8, name: 'Ba Sing Se', url: 'https://theartofthestorydotcom.files.wordpress.com/2015/01/korra_basingse_outerring.jpg'},
-  {id: 1000, name: 'New New York', url: 'http://vignette2.wikia.nocookie.net/en.futurama/images/5/55/160_planet-express-building_1024.png/revision/latest?cb=20090521203416'},
-  {id: 9, name: 'Seattle', url: 'http://www.sustainableseattle.org/wp-content/uploads/2015/08/SeattleMunicipleArchive-e1488676312333.jpg'},
-  {id: 10, name: 'Rome', url: 'https://brightcove04pmdo-a.akamaihd.net/5104226627001/5104226627001_5232386545001_5215063851001-vs.jpg?pubId=5104226627001&videoId=5215063851001'},
-  {id: 11, name: 'Phoenix', url: 'http://www.beautyschoolsdirectory.com/sites/default/files/2017-03/Phoenix%2C%20AZ.jpg'}
+  {id: 1, name: 'Los Angeles', url: 'https://s3.amazonaws.com/cws-dev/users/images/000/Cities/los_angeles.jpeg'},
+  {id: 2, name: 'New York City', url: 'https://s3.amazonaws.com/cws-dev/users/images/000/Cities/new_york.jpg'},
+  {id: 3, name: 'Honolulu', url: 'https://s3.amazonaws.com/cws-dev/users/images/000/Cities/honolulu.jpg'},
+  {id: 4, name: 'Paris', url: 'https://s3.amazonaws.com/cws-dev/users/images/000/Cities/paris.jpg'},
+  {id: 5, name: 'Miami', url: 'https://s3.amazonaws.com/cws-dev/users/images/000/Cities/miami.jpg'},
+  {id: 6, name: 'Berlin', url: 'https://s3.amazonaws.com/cws-dev/users/images/000/Cities/berlin.jpg'},
+  {id: 7, name: 'Gravity Falls', url: 'https://s3.amazonaws.com/cws-dev/users/images/000/Cities/gravity_falls.jpg'},
+  {id: 8, name: 'Ba Sing Se', url: 'https://s3.amazonaws.com/cws-dev/users/images/000/Cities/ba_sing_se.jpg'},
+  {id: 1000, name: 'New New York', url: 'https://s3.amazonaws.com/cws-dev/users/images/000/Cities/new_new_york.png'},
+  {id: 9, name: 'Seattle', url: 'https://s3.amazonaws.com/cws-dev/users/images/000/Cities/seattle.jpg'},
+  {id: 10, name: 'Rome', url: 'https://s3.amazonaws.com/cws-dev/users/images/000/Cities/rome.jpg'},
+  {id: 11, name: 'Phoenix', url: 'https://s3.amazonaws.com/cws-dev/users/images/000/Cities/phoenix.jpg'}
 ];
 
 export default DUMMY_CITIES;
