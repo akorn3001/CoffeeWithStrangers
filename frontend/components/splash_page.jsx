77c4777f736097b9751fa1aeb2f0a2ad097fdb69
Alex Kornfeld <akorn3000@gmail.com>
2017-12-04 14:29:48 -0500

    Build out splash page

--- frontend/components/splash_page.jsx
+++ frontend/components/splash_page.jsx
@@ -4,13 +4,22 @@ class SplashPage extends React.Component {
   componentDidMount() {
     window.scrollTo(0, 0);
   }
-  
+
   render () {
     return (
       <div>
         <div className="splash-banner">
           <img src={window.staticImages.aboutBanner}/>
           <span>Coffee and Conversation Awaits You</span>
+        </div>
+
+        <div className="below-splash-banner">
+          <img src={window.staticImages.coffeeFaces}/>
+          <blockquote>
+            "A leaf fluttered in through the window this morning, as if supported
+            by the rays of the sun, a bird settled on the fire escape, joy in the
+            task of coffee, joy accompanied me as I walked" ~ Anais Nin
+          </blockquote>
         </div>
       </div>
     );
